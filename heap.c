adec8f8a795bec3cc0d13a36d03ba5d7a8ffdc97
Replit user <>
2023-05-11 00:06:17 +0000

    new try

--- heap.c
+++ heap.c
@@ -1,38 +1,35 @@
+#include "heap.h"
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include "heap.h"
 
-typedef struct nodo{
-   void* data;
-   int priority;
-}heapElem;
+typedef struct nodo {
+  void *data;
+  int priority;
+} heapElem;
 
-typedef struct Heap{
-  heapElem* heapArray;
+typedef struct Heap {
+  heapElem *heapArray;
   int size;
   int capac;
 } Heap;
 
+void *heap_top(Heap *pq) {
+  if (pq == NULL || pq->size == 0)
+    return NULL;
 
-void* heap_top(Heap* pq)
-{
-  if( pq == NULL || pq->size == 0) return NULL;
-  
   return pq->heapArray[0].data;
 }
 
+void heap_push(Heap *pq, void *data, int priority) {
+  if (pq == NULL || pq->size == 0)
+    return;
 
-
-void heap_push(Heap* pq, void* data, int priority)
-{
-    if( pq == NULL || pq->size == 0) return;
-
-  if( pq->size == pq->capac) // si esta lleno
+  if (pq->size == pq->capac) // si esta lleno
   {
-    int nueva_capacidad = (2* pq->capac) + 1;
+    int nueva_capacidad = (2 * pq->capac) + 1;
     pq->heapArray = realloc(pq->heapArray, nueva_capacidad * sizeof(heapElem));
     pq->capac = nueva_capacidad;
   }
@@ -41,8 +38,10 @@ void heap_push(Heap* pq, void* data, int priority)
   pq->heapArray[i].data = data;
   pq->heapArray[i].priority = priority;
   // organizar array
-  while (i > 0 && pq->heapArray[i].priority > pq->heapArray[(i - 1) / 2].priority) 
-  // Mientras que el indice no sea la raiz y que la prioridad del nuevo dato no sea mayor que la del padre
+  while (i > 0 &&
+         pq->heapArray[i].priority > pq->heapArray[(i - 1) / 2].priority)
+  // Mientras que el indice no sea la raiz y que la prioridad del nuevo dato no
+  // sea mayor que la del padre
   {
     heapElem temp = pq->heapArray[i];
     pq->heapArray[i] = pq->heapArray[(i - 1) / 2];
@@ -52,19 +51,17 @@ void heap_push(Heap* pq, void* data, int priority)
   pq->size++;
 }
 
-
-void heap_pop(Heap* pq)
-{
-  if( pq == NULL || pq->size == 0) return;
+void heap_pop(Heap *pq) {
+  if (pq == NULL || pq->size == 0)
+    return;
   pq->size--;
   pq->heapArray[0] = pq->heapArray[pq->size];
 }
 
-Heap* createHeap()
-{
+Heap *createHeap() {
   Heap *dato = malloc(sizeof(Heap));
-  dato->capac=3;
-  dato->size=0;
-  dato->heapArray = malloc(3 *sizeof(heapElem));
+  dato->capac = 3;
+  dato->size = 0;
+  dato->heapArray = malloc(3 * sizeof(heapElem));
   return dato;
 }
